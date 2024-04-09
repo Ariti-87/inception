8e36cb4ad5302bedd85f278d484f2d674ca7f138
arincon <ariti87@hotmail.com>
2024-04-09 18:34:17 +0200

    fin

--- srcs/requirements/mariadb/conf/entrypoint.sh
+++ srcs/requirements/mariadb/conf/entrypoint.sh
@@ -5,7 +5,7 @@ service mariadb start;
 sleep 1
 
 #mariadb -e "UPDATE mysql.user SET Password = PASSWORD('${SQL_ROOT_PASSWORD}') WHERE User = 'root';"
-mariadb -e "CREATE DATABASE IF NOT EXISTS ${SQL_DATABASE};"
+mariadb -e "CREATE DATABASE IF NOT EXISTS \`${SQL_DATABASE}\`;"
 mariadb -e "CREATE USER IF NOT EXISTS \`${SQL_USER}\`@'localhost' IDENTIFIED BY '${SQL_PASSWORD}';"
 mariadb -e "GRANT ALL PRIVILEGES ON *.* TO \`root\`@'%' IDENTIFIED BY '${SQL_ROOT_PASSWORD}';"
 mariadb -e "GRANT ALL PRIVILEGES ON ${SQL_DATABASE}.* TO \`${SQL_USER}\`@'%' IDENTIFIED BY '${SQL_PASSWORD}';"
--- srcs/requirements/wordpress/conf/entrypoint.sh
+++ srcs/requirements/wordpress/conf/entrypoint.sh
@@ -3,25 +3,25 @@
 sleep 10
 # cd /var/www/wordpress
 
-./wp-cli.phar config create --path=/var/www/wordpress --allow-root \
-                            --dbname=$SQL_DATABASE \
-                            --dbuser=$SQL_USER \
-                    --dbpass=$SQL_PASSWORD \
-                    --dbhost=mariadb:3306
+./wp-cli.phar config create     --path=/var/www/wordpress --allow-root \
+                                --dbname=$SQL_DATABASE \
+                                --dbuser=$SQL_USER \
+                                --dbpass=$SQL_PASSWORD \
+                                --dbhost=mariadb:3306
 
-./wp-cli.phar core install     --path=/var/www/wordpress --allow-root \
-                    --url=https://arincon.42.fr \
-                    --title=$WP_TITLE \
-                    --admin_user=$WP_ADMIN_USER \
-                    --admin_password=$WP_ADMIN_PASSWORD \
-                    --admin_email=$WP_ADMIN_EMAIL \
-                    --skip-email
+./wp-cli.phar core install      --path=/var/www/wordpress --allow-root \
+                                --url=https://arincon.42.fr \
+                                --title=$WP_TITLE \
+                                --admin_user=$WP_ADMIN_USER \
+                                --admin_password=$WP_ADMIN_PASSWORD \
+                                --admin_email=$WP_ADMIN_EMAIL \
+                                --skip-email
 
 
-./wp-cli.phar user create  --path=/var/www/wordpress --allow-root \
-                $WP_USER_LOGIN \
-                $WP_USER_EMAIL \
-                --user_pass=$WP_USER_PASSWORD
+./wp-cli.phar user create       --path=/var/www/wordpress --allow-root \
+                                $WP_USER_LOGIN \
+                                $WP_USER_EMAIL \
+                                --user_pass=$WP_USER_PASSWORD
 
 mkdir -p /run/php
 
